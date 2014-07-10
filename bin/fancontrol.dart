c16b9ff3f0d6954b1177153c8e473882432cc35f
k4rr1m <k4rr.1m@gmail.com>
2014-07-10 16:54:17 +0200

    Initial commit

---
+++ bin/fancontrol.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+void main () {
+  Map<String, int> conf = JSON.decoder.convert(new File('../conf.d/fancontrol.json').readAsStringSync());
+  Map<String, int> fans = <String, int>{};
+  RegExp regex = new RegExp(r'GPU \d\d\d\d:\d(\d):\d\d\.\d\s*Temperature\s*Gpu\s*: (\d\d) C', multiLine: true, caseSensitive: true);
+  new Timer.periodic(new Duration(seconds: 2), (timer) {
+    Process.run('nvidia-smi', <String>['-q', '-d', 'TEMPERATURE']).then((result) {
+      if (result.stdout is String) {
+        regex.allMatches(result.stdout).forEach((match) {
+          String id = match[1];
+          int speed, temp = int.parse(match[2]), targettemp = conf[id];
+          if (fans.containsKey(id)) {
+            speed = fans[id];
+          } else {
+            fans[id] = speed = 50;
+          }          
+          speed += (temp - targettemp);
+          if (speed > 100) {
+            speed = 100;
+          } else if (speed < 0) {
+            speed = 0;
+          }
+          fans[id] = speed;
+          Process.run('nvidia-settings', <String>['-a',
+                                                  '"[gpu:$id]/GPUFanControlState=1"',
+                                                  '-a',
+                                                  '[fan:$id]/GPUCurrentFanSpeed=$speed'
+                                                  ]).then((result) {
+            if (result.stdout is String) {
+              print ('[fan:$id] Temp $temp° Fan $speed%');
+            } else {
+              print ('${result.stderr}');
+            }
+          });
+        });
+      }
+    });
+  });
+}
